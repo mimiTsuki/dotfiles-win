37a16a8eff5dc7f88b86e89dd13b54252ad695df
mimiTsuki <okajima02k@gmail.com>
2022-02-19 01:51:22 +0900

    init.shをmac用に変更

--- zsh/init.sh
+++ zsh/init.sh
@@ -6,15 +6,6 @@ ln dotfiles/zsh/.zshrc ~/.zshrc
 
 # homebrewのインストール
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-# zshのインストール
-sudo apt-get install zsh
-
-chsh -s $(which zsh)
-
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
 
 # Zinitのインストール
 
